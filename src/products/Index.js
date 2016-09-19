89357c4ddfdfdf564bfcda4b0a33b0e95fce79f6
marcuscaum <marcuslucas.jb@gmail.com>
2016-09-19 10:50:03 -0300

    removed local url

--- src/products/Index.js
+++ src/products/Index.js
@@ -4,10 +4,10 @@ import './Index.css';
 
 class Index extends Component {
   render() {
-    const SOURCE_URL = 'http://localhost:3010/products/all' || 'https://react-store-api.herokuapp.com/products/all'
+    // const SOURCE_URL = 'http://localhost:3010/products/all' || 'https://react-store-api.herokuapp.com/products/all'
 
     return (
-      <ProductsList source={SOURCE_URL}/>
+      <ProductsList source="https://react-store-api.herokuapp.com/products/all"/>
     );
   }
 }
