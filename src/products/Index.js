02febabd69bf7f6b0ed773b55a2a6bd714f57f8e
marcuscaum <marcuslucas.jb@gmail.com>
2016-09-29 18:52:32 -0300

    added colorpicker and size manager

--- src/products/Index.js
+++ src/products/Index.js
@@ -4,10 +4,8 @@ import './Index.css';
 
 class Index extends Component {
   render() {
-    // const SOURCE_URL = 'http://localhost:3010/products/all' || 'https://react-store-api.herokuapp.com/products/all'
-
     return (
-      <ProductsList source="https://react-store-api.herokuapp.com/products/all"/>
+      <ProductsList/>
     );
   }
 }
--- src/products/New.js
+++ src/products/New.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Col, FormGroup, FormControl, ControlLabel, InputGroup, Button } from 'react-bootstrap';
+import { Row, Col, FormGroup, FormControl, ControlLabel, InputGroup, Button, ButtonGroup, ButtonToolbar } from 'react-bootstrap';
 import Firebase from 'firebase';
 import Dropzone from 'react-dropzone';
+import { ChromePicker } from 'react-color';
 
 class New extends Component {
 
@@ -11,8 +12,14 @@ class New extends Component {
       name: '',
       description: '',
       price: '',
-      images: ''
+      images: [],
+      options: {
+        colors: [],
+        sizes: []
+      },
+      displayColorPicker: false
     }
+    window.state = this.state;
   }
 
   getValidationState(state) {
@@ -22,61 +29,128 @@ class New extends Component {
     else if (length > 0) return 'error';
   }
 
-  onDropImage(files) {
+  onDropImage = (files) => {
     let images = this.state.images || [];
 
     images.push(files);
-
     this.setState({
       images: images
     });
   }
 
+  onSelectColor = (color) => {
+    let colors = this.state.options.colors || [];
+    if (colors.length === 5) {
+      return this.setState({displayColorPicker: false})
+    };
+
+    colors.push(color.hex);
+    this.setState({
+      ...this.state.options,
+      colors: colors
+    })
+  }
+
+  sizeButtons() {
+    return(
+      <div>
+        <Button active={(this.state.options.sizes.indexOf("P") > -1)} onClick={this.handleSizes.bind(this, "P")}>P</Button>
+        <Button active={(this.state.options.sizes.indexOf("M") > -1)} onClick={this.handleSizes.bind(this, "M")}>M</Button>
+        <Button active={(this.state.options.sizes.indexOf("G") > -1)} onClick={this.handleSizes.bind(this, "G")}>G</Button>
+        <Button active={(this.state.options.sizes.indexOf("GG") > -1)} onClick={this.handleSizes.bind(this, "GG")}>GG</Button>
+        <Button active={(this.state.options.sizes.indexOf("XGG") > -1)} onClick={this.handleSizes.bind(this, "XGG")}>XGG</Button>
+      </div>
+    )
+  }
+
+  handleSizes(size) {
+    let sizes = this.state.options.sizes || [];
+    let sizeIndex = sizes.indexOf(size);
+
+    if (sizeIndex > -1) {
+      sizes.splice(sizeIndex, 1)
+      this.setState({
+        ...this.state.options,
+        sizes: sizes
+      })
+    } else {
+      sizes.push(size);
+      this.setState({
+        ...this.state.options,
+        sizes: sizes
+      })
+    }
+  }
+
   render() {
+
     return (
       <form>
         <legend> Adicionar um novo produto</legend>
-        <Col md={6}>
-          <FormGroup validationState={this.getValidationState(this.state.name)}>
-            <ControlLabel>Nome do Produto:</ControlLabel>
-            <FormControl
-              type="text"
-              value={this.state.name}
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-            <FormControl.Feedback />
-          </FormGroup>
-          <FormGroup validationState={this.getValidationState(this.state.description)}>
-            <ControlLabel>Descrição:</ControlLabel>
-            <FormControl
-                componentClass="textarea"
-                value={this.state.description}
-                onChange={(e) => this.setState({ description: e.target.value })}
-                rows="5"
-            />
-            <FormControl.Feedback />
-          </FormGroup>
-          <Button bsStyle="success" onClick={this.createNewProduct}> Adicionar produto</Button>
-        </Col>
-        <Col md={6}>
-          <FormGroup>
-            <ControlLabel>Valor:</ControlLabel>
-            <InputGroup>
-              <InputGroup.Addon>R$</InputGroup.Addon>
-              <FormControl type="text"
-                value={this.state.price}
-                onChange={(e) => this.setState({ price: e.target.value })}
+        <Row>
+          <Col md={6}>
+            <FormGroup validationState={this.getValidationState(this.state.name)}>
+              <ControlLabel>Nome do Produto:</ControlLabel>
+              <FormControl
+                type="text"
+                value={this.state.name}
+                onChange={(e) => this.setState({ name: e.target.value })}
               />
-              <InputGroup.Addon>.00</InputGroup.Addon>
-            </InputGroup>
-          </FormGroup>
-          <FormGroup>
-            <ControlLabel>Imagem do produto:</ControlLabel>
-            <Dropzone onDrop={this.onDropImage.bind(this)}>
-              <div>Try dropping some files here, or click to select files to upload.</div>
-            </Dropzone>
-          </FormGroup>
-        </Col>
+              <FormControl.Feedback />
+            </FormGroup>
+            <FormGroup validationState={this.getValidationState(this.state.description)}>
+              <ControlLabel>Descrição:</ControlLabel>
+              <FormControl
+                  componentClass="textarea"
+                  value={this.state.description}
+                  onChange={(e) => this.setState({ description: e.target.value })}
+                  rows="5"
+              />
+              <FormControl.Feedback />
+            </FormGroup>
+          </Col>
+          <Col md={6} onMouseLeave={() => this.setState({displayColorPicker: false})}>
+            <FormGroup>
+              <ControlLabel>Valor:</ControlLabel>
+              <InputGroup>
+                <InputGroup.Addon>R$</InputGroup.Addon>
+                <FormControl type="text"
+                  value={this.state.price}
+                  onChange={(e) => this.setState({ price: e.target.value })}
+                />
+                <InputGroup.Addon>.00</InputGroup.Addon>
+              </InputGroup>
+            </FormGroup>
+            <FormGroup >
+              <ControlLabel>Cores:</ControlLabel>
+              <div className="colorpicker-tiles" onClick={() => this.setState({displayColorPicker: true})}>
+                {this.state.options.colors.map((color, key) => <div key={key} style={{backgroundColor: color}}></div> )}
+              </div>
+              { this.state.displayColorPicker ? <ChromePicker  onMouseLeave={() => this.setState({displayColorPicker: false})} color={this.state.options.colors} onChangeComplete={this.onSelectColor}/> : null }
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Tamanhos:</ControlLabel>
+              <ButtonToolbar>
+                <ButtonGroup>
+                  {this.sizeButtons()}
+                </ButtonGroup>
+              </ButtonToolbar>
+            </FormGroup>
+            <FormGroup>
+              <ControlLabel>Adicionar imagens:</ControlLabel>
+              <Dropzone className="image-dropzone" onDrop={this.onDropImage}>
+                <div>Try dropping some files here, or click to select files to upload.</div>
+              </Dropzone>
+              {this.state.images.length > 0 ? <div>
+              <h2>Uploading {this.state.images.length} files...</h2>
+              <div>{this.state.images.map((image, key) => <img key={key} src={image[0].preview} role="presentation"/> )}</div>
+              </div> : null}
+            </FormGroup>
+          </Col>
+        </Row>
+        <ButtonGroup>
+          <Button bsStyle="success" onClick={this.createNewProduct}> Adicionar produto</Button>
+        </ButtonGroup>
       </form>
     );
   }
